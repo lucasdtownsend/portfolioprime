9815d5f148b8af8a2dd72ce82e4ec617a1ec8fd8
Lucas Townsend <lucasdtownsend@gmail.com>
2015-11-25 00:00:44 -0500

    my masterpiece

---
+++ TownsendBetterPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TownsendBetterPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult frms()
+        {
+            return View();
+        }
+
+        public ActionResult nofrms()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        public ActionResult aboutgate()
+        {
+            return View();
+        }
+
+        public ActionResult projects()
+        {
+            return View();
+        }
+
+        public ActionResult projectsgate()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+        public ActionResult contactgate()
+        {
+            return View();
+        }
+
+    }
+}
